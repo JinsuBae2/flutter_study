15b804a3d55e3b86bfbafdf8c3a417873044d73d
JinsuBae2 <bjs7898@naver.com>
2024-09-25 17:13:49 +0900

    lottie animation and scroll add

--- codingchef_study/lib/lottery_app/lotto.dart
+++ codingchef_study/lib/lottery_app/lotto.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 import 'dart:math';
 
 import 'package:login_app/lottery_app/lotto_ball.dart';
+import 'package:lottie/lottie.dart';
 
 class Lotto extends StatefulWidget {
   const Lotto({super.key});
@@ -40,46 +42,65 @@ class _LottoState extends State<Lotto> {
         centerTitle: true,
       ),
       body: Center(
-        child: ListView.builder(
-          padding: const EdgeInsets.all(8),
-          itemCount: listLottery.length,
-          itemBuilder: (BuildContext context, int index) {
-            var path_1 = 'svg/${listLottery[index][0]}.svg';
-            var path_2 = 'svg/${listLottery[index][1]}.svg';
-            var path_3 = 'svg/${listLottery[index][2]}.svg';
-            var path_4 = 'svg/${listLottery[index][3]}.svg';
-            var path_5 = 'svg/${listLottery[index][4]}.svg';
-            var path_6 = 'svg/${listLottery[index][5]}.svg';
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  LottoBall(path: path_1),
-                  const SizedBox(
-                    width: 5,
-                  ),
-                  LottoBall(path: path_2),
-                  const SizedBox(
-                    width: 5,
-                  ),
-                  LottoBall(path: path_3),
-                  const SizedBox(
-                    width: 5,
-                  ),
-                  LottoBall(path: path_4),
-                  const SizedBox(
-                    width: 5,
-                  ),
-                  LottoBall(path: path_5),
-                  const SizedBox(
-                    width: 5,
-                  ),
-                  LottoBall(path: path_6),
-                ],
+        child: Column(
+          children: [
+            Expanded(
+              child: RawScrollbar(
+                thumbColor: const Color.fromARGB(255, 122, 157, 218),
+                thumbVisibility: true,
+                trackVisibility: true,
+                thickness: 15,
+                radius: const Radius.circular(10),
+                interactive: true,
+                child: ListView.builder(
+                  padding: const EdgeInsets.all(8),
+                  itemCount: listLottery.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    var path_1 = 'svg/${listLottery[index][0]}.svg';
+                    var path_2 = 'svg/${listLottery[index][1]}.svg';
+                    var path_3 = 'svg/${listLottery[index][2]}.svg';
+                    var path_4 = 'svg/${listLottery[index][3]}.svg';
+                    var path_5 = 'svg/${listLottery[index][4]}.svg';
+                    var path_6 = 'svg/${listLottery[index][5]}.svg';
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          LottoBall(path: path_1),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          LottoBall(path: path_2),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          LottoBall(path: path_3),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          LottoBall(path: path_4),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          LottoBall(path: path_5),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          LottoBall(path: path_6),
+                        ],
+                      ),
+                    );
+                  },
+                ),
               ),
-            );
-          },
+            ),
+            SizedBox(
+              width: 150,
+              height: 150,
+              child: Lottie.asset('lottie/dog.json'),
+            ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
